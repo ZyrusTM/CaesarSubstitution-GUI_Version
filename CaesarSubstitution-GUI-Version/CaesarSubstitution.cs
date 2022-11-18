42a33762113677b602b4fd08b7b561323db9980c
Erik Bommes <erik@hahn-berlin.net>
2022-11-18 01:06:12 +0100

    initial commit

---
+++ CaesarSubstitution-GUI-Version/CaesarSubstitution.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CaesarSubstitution_GUI_Version
+{
+    internal class CaesarSubstitution
+    {
+        private char[] _textArray;
+        private int _key;
+
+        public void SetData(string text, int key)
+        {
+            if (_textArray != null)
+            {
+                Array.Clear(_textArray, 0, _textArray.Length);
+            }
+
+            text = Regex.Replace(text, " ", "");
+            _textArray = ToUpperCase(text).ToCharArray();
+            _key = key;
+        }
+
+        private string ToUpperCase(string text)
+        {
+            string InUpper = "";
+
+            foreach (char c in text)
+            {
+                if ((int)c >= 97)
+                {
+                    int UpperCaseAscii = (int)c - 32;
+                    InUpper += (char)UpperCaseAscii;
+                }
+                else
+                {
+                    InUpper += c;
+                }
+            }
+            return InUpper;
+        }
+
+        public string Encrypt()
+        {
+            string encText = "";
+            foreach (char c in _textArray)
+            {
+                var ascii = (int)c;
+                ascii -= 65;
+                int encAscii = (ascii + _key) % 26;
+                encAscii += 65;
+                encText += (char)encAscii;
+            }
+            return encText;
+        }
+
+        public string Decrypt()
+        {
+            string decText = "";
+
+            foreach (char c in _textArray)
+            {
+                var ascii = (int)c;
+                ascii -= 65;
+                int decAscii = (ascii - _key) % 26;
+                if (decAscii < 0)
+                {
+                    decAscii += 26;
+                }
+                decAscii += 65;
+                decText += (char)decAscii;
+            }
+            return decText;
+        }
+    }
+}
---
+++ CaesarSubstitution-GUI-Version/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CaesarSubstitution_GUI_Version
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string ButtonText;
+        public ICommand ButtonTextCommand;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ButtonText = "Encrypt";
+
+        }
+
+        public void AddOutputText()
+        {
+            
+        }
+
+        private void CheckboxEncrypt_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
